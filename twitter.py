14d4bf82f9a50c5d2bed86c2a00cfc8ccdc0d9a2
wodim <wodim@vortigaunt.net>
2023-06-10 02:38:00 +0200

    deduplicate twitter feeds

--- twitter.py
+++ twitter.py
@@ -5,15 +5,21 @@ from tweepy import API, OAuthHandler
 from utils import _config, is_admin, logger
 
 
-# TODO this should be a dict "username -> [channels]" to deduplicate feeds
 def _get_twitter_feeds() -> dict:
     if feeds := _config('twitter_feeds'):
-        return [(x, int(y)) for x, y in [x.strip().split('|') for x in feeds.split(',')]]
-    return []
+        ret = {}
+        for feed in feeds.split(','):
+            screen_name, chat_id = feed.strip().split('|')
+            try:
+                ret[screen_name.lower()].append(int(chat_id))
+            except KeyError:
+                ret[screen_name.lower()] = [int(chat_id)]
+        return ret.items()
+    return {}
 
 
-def _create_link(username: str, id_: int) -> str:
-    return f'https://vxtwitter.com/{username}/status/{id_}'
+def _create_link(screen_name: str, id_: int) -> str:
+    return f'https://vxtwitter.com/{screen_name}/status/{id_}'
 
 
 auth = OAuthHandler(_config('twitter_consumer_key'), _config('twitter_consumer_secret'))
@@ -22,18 +28,18 @@ api = API(auth)
 
 
 def cron_twitter(context: CallbackContext) -> None:
-    for username, chat_id in _get_twitter_feeds():
+    for screen_name, chat_ids in _get_twitter_feeds():
         try:
-            last_tweets = api.user_timeline(screen_name=username)
+            last_tweets = api.user_timeline(screen_name=screen_name)
         except Exception as exc:
-            logger.info('Error retrieving tweets from "%s": %s', username, exc)
+            logger.info('Error retrieving tweets from "%s": %s', screen_name, exc)
             continue
         if not last_tweets:
             continue
 
-        last_id = context.bot_data['last_tweet_ids'].get(username)
+        last_id = context.bot_data['last_tweet_ids'].get(screen_name)
         # last_tweets[0] won't crash here because we already checked for last_tweets earlier
-        context.bot_data['last_tweet_ids'][username] = last_tweets[0].id
+        context.bot_data['last_tweet_ids'][screen_name] = last_tweets[0].id
 
         if last_id:  # will be False the first time this is run so we don't post anything
             for tweet in last_tweets[::-1]:
@@ -42,17 +48,18 @@ def cron_twitter(context: CallbackContext) -> None:
                     not hasattr(tweet, 'retweeted_status') or
                     tweet.retweeted_status.user.screen_name != tweet.user.screen_name
                 ):
-                    url = _create_link(username, tweet.id)
+                    url = _create_link(screen_name, tweet.id)
                     logger.info('Posting %s', url)
-                    context.bot.send_message(chat_id, url)
+                    for chat_id in chat_ids:
+                        context.bot.send_message(chat_id, url)
 
 
 def command_twitter(update: Update, context: CallbackContext) -> None:
     """shows latest saved tweets for each account we're watching"""
     if is_admin(update.message.from_user.id):
         if context.bot_data['last_tweet_ids']:
-            update.message.reply_text('\n'.join([f'{username}: {_create_link(username, id_)}'
-                                                for username, id_
+            update.message.reply_text('\n'.join([f'{screen_name}: {_create_link(screen_name, id_)}'
+                                                for screen_name, id_
                                                 in context.bot_data['last_tweet_ids'].items()]),
                                     disable_web_page_preview=True)
         else:
